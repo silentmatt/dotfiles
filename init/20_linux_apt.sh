36a6f83ab6c24f3a166e06b033598b669776ca18
Matthew Crumley <email@matthewcrumley.com>
2020-08-25 00:31:19 -0400

    Skip apt upgrade

--- init/20_linux_apt.sh
+++ init/20_linux_apt.sh
@@ -308,12 +308,12 @@ e_header "Updating APT"
 sudo apt-get -qq update
 
 # Only do a dist-upgrade on initial install, otherwise do an upgrade.
-e_header "Upgrading APT"
-if is_dotfiles_bin; then
-  sudo apt-get -qy upgrade
-else
-  sudo apt-get -qy dist-upgrade
-fi
+#e_header "Upgrading APT"
+#if is_dotfiles_bin; then
+#  sudo apt-get -qy upgrade
+#else
+#  sudo apt-get -qy dist-upgrade
+#fi
 
 # Install APT packages.
 installed_apt_packages="$(dpkg --get-selections | grep -v deinstall | awk 'BEGIN{FS="[\t:]"}{print $1}' | uniq)"
